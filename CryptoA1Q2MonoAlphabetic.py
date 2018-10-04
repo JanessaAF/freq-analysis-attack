3485853667399e676b62f7bda1144328daffe6fc
unknown <janessa.af@gmail.com>
2018-10-04 17:36:31 -0500

    shift cipher attack nearly completed percentage changed to decimal to facilitate easier math

--- CryptoA1Q2MonoAlphabetic.py
+++ CryptoA1Q2MonoAlphabetic.py
@@ -4,8 +4,8 @@ class Frequency:
         self.frequency = frequency
     def increment(self):
         self.frequency += 1
-    def convertToPercentage(self, totalCharacters):
-        self.frequency = (self.frequency / totalCharacters) * 100
+    def convertToDecimal(self, totalCharacters):
+        self.frequency = (self.frequency / totalCharacters)
     def equals(self, newLetter):
         return self.letter == newLetter
     def __str__(self):
@@ -14,34 +14,35 @@ class Frequency:
         return self.letter + ': ' + str(self.frequency)
 
 MODE = "SHIFT" #"VIGENERE"/"SHIFT"/"MONOALPHABETIC"
+FREQUENCY_SUM_OF_SQUARES = 0.065
 
 ENGLISH_LETTER_FREQUENCIES = [
-    Frequency('e', 12.7),
-    Frequency('t', 9.1),
-    Frequency('a', 8.2),
-    Frequency('o', 7.5),
-    Frequency('i', 7.0),
-    Frequency('n', 6.7),
-    Frequency('s', 6.3),
-    Frequency('h', 6.1),
-    Frequency('r', 6.0),
-    Frequency('d', 4.3),
-    Frequency('l', 4.0),
-    Frequency('c', 2.8),
-    Frequency('u', 2.8),
-    Frequency('w', 2.4),
-    Frequency('m', 2.4),
-    Frequency('f', 2.2),
-    Frequency('y', 2.0),
-    Frequency('g', 2.0),
-    Frequency('p', 1.9),
-    Frequency('b', 1.5),
-    Frequency('v', 1.0),
-    Frequency('k', 0.8),
-    Frequency('j', 0.2),
-    Frequency('x', 0.2),
-    Frequency('z', 0.1),
-    Frequency('q', 0.1)
+    Frequency('e', 0.127),
+    Frequency('t', 0.091),
+    Frequency('a', 0.082),
+    Frequency('o', 0.075),
+    Frequency('i', 0.070),
+    Frequency('n', 0.067),
+    Frequency('s', 0.063),
+    Frequency('h', 0.061),
+    Frequency('r', 0.060),
+    Frequency('d', 0.043),
+    Frequency('l', 0.040),
+    Frequency('c', 0.028),
+    Frequency('u', 0.028),
+    Frequency('w', 0.024),
+    Frequency('m', 0.024),
+    Frequency('f', 0.022),
+    Frequency('y', 0.020),
+    Frequency('g', 0.020),
+    Frequency('p', 0.019),
+    Frequency('b', 0.015),
+    Frequency('v', 0.010),
+    Frequency('k', 0.008),
+    Frequency('j', 0.002),
+    Frequency('x', 0.002),
+    Frequency('z', 0.001),
+    Frequency('q', 0.001)
 ]
 
 CIPHERTEXT = "JGRMQOYGHMVBJWRWQFPWHGFFDQGFPFZRKBEEBJIZQQOCIBZKLFAFGQVFZFWWEOGWOPFGFHWOLPHLRLOLFDMFGQWBLWBWQOLKFWBYLBLYLFSFLJGRMQBOLWJVFPFWQVHQWFFPQOQVFPQOCFPOGFWFJIGFQVHLHLROQVFGWJVFPFOLFHGQVQVFILEOGQILHQFQGIQVVOSFAFGBWQVHQWIJVWJVFPFWHGFIWIHZZRQGBABHZQOCGFHX"
@@ -105,14 +106,70 @@ def monoAlphabetic():
     print()
     print(''.join(plaintext))
 
-#shift and monoalphabetic shouldnt make a difference?
-def shift():
 
+def sumOfSquares(array):
+    result = 0;
+    for i in array:
+        result = result + (array[i]**2)
+    return result
+#shift and monoalphabetic shouldnt make a difference?
+
+def shiftLetters(stream, offset):
+    newStream = ''
+    for i in stream:
+        asciiVal = ord(stream[i]) + offset
+        if asciiVal > ord('z'):
+            asciiVal = asciiVal - ord('z') % 26 + (ord('a') - 1)
+        newStream[i] = chr(asciiVal)
+    return newStream
+
+def findLetter(char):
+    for charFrequency in ENGLISH_LETTER_FREQUENCIES:
+        if char == str.lower(charFrequency.letter)
+            return charFrequency
+
+def shiftCipher(stream):
+    closestFreqOffset = 0
+    closestFreq = 1
+
+    #for all j (potential offset aka potential value of k)
+    for j in range(0,25):
+
+        shiftedStream = shiftLetters(stream, j)
+        #get frequency
+        for i in shiftedStream:
+            for j in frequencyList:
+                if j.equals(i):
+                    j.increment()
+
+        print(frequencyList)
+
+        for j in frequencyList:
+            j.convertToDecimal(len(stream))
+        #calculate i sub j for every letter?
+        frequencyCalc = 0
+        for char in frequencyList:
+            equivChar = findLetter(char.letter)
+            frequencyCalc = frequencyCalc + (equivChar.frequency * char.frequency)
+        #if calculation is closer to the frequency of english letters sum of squares
+        # keep track of that calculation and our current best option for the shift
+        if FREQUENCY_SUM_OF_SQUARES - abs(frequencyCalc) < closesetFreq:
+            closestFreq = FREQUENCY_SUM_OF_SQUARES - abs(frequencyCalc)
+            closestFreqOffset = j
+    #return most likely value
+    return closestFreqOffset
 
 if MODE == "MONOALPHABETIC":
     monoAlphabetic()
-else:
-    if MODE == "SHIFT":
-        #build stream
-    for
+elif MODE === "VIGENERE":
+    #for each possible key length 1 to m length
+    #for each
+    streamArr = "";
+    #split streams
+        #need to increment for vigenere though: key length = 1,2,3,4...
+    for stream in streamArr:
         shift(stream)
+    #generate sum of squares
+elif MODE === "SHIFT":
+    streamArr == "this is a sentence"
+    shiftCipher(streamArr)
